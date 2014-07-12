c9ebda91219baab7c07a13df4758e33a0e902222
ctrl.consult <suporte@control.com.br>
2014-07-12 12:17:32 -0300

    iAH
    Diretório com iAH.

---
+++ htdocs/iah/scripts/iah.def.php
@@ -0,0 +1,26 @@
+<?php
+[PATH]
+
+PATH_DATA=/iah/
+PATH_CGI-BIN=/var/www/abcd/htdocs/iah/scripts/
+PATH_DATABASE=/var/www/abcd/bases/
+PATH_DEF=/var/www/abcd/bases/par/
+
+[APPEARANCE]
+
+/* Please adjust /css/stylesheet.css */
+
+[HEADER]
+
+LOGO IMAGE=logoabcd.jpg
+LOGO URL=^2http://www.google.com
+HEADER IMAGE=online.gif
+HEADER URL=^2/iah/en/index.htm
+
+[IAH]
+
+MANAGER E-MAIL=ia@hmy-server.domain
+REVERSE MODE=ON
+MULTI-LANGUAGE=ON
+AVAILABLE LANGUAGES=pt,es,en,fr
+?>
