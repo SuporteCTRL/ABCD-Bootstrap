762d555bcaf60ae007f97dd078ddf559a92c72e5
Suporte <suporte@control.com.br>
2014-07-13 01:12:33 -0300

    favicon

--- htdocs/central/common/header.php
+++ htdocs/central/common/header.php
@@ -1,25 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
 		<title><?php echo $def["LEGEND2"]; ?></title>
 		<meta http-equiv="Expires" content="-1" />
 		<meta http-equiv="pragma" content="no-cache" />
 <?php
-	if (isset($cisis_ver) and $cisis_ver=="unicode/"){		//echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1256\" />";
-		echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";	}else{		echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\" />";	}
+	if (isset($cisis_ver) and $cisis_ver=="unicode/"){		echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";	}else{		echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\" />";	}
 ?>
-		<meta name="robots" CONTENT="NONE" />
-		<meta http-equiv="keywords" content="" />
-		<meta http-equiv="description" content="" />
-		<!-- Stylesheets -->
+	<meta name="robots" CONTENT="NONE" />
+	<meta http-equiv="keywords" content="" />
+	<meta http-equiv="description" content="" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/central/tema/css/bootstrap.min.css">
 
- 
-		
-        <link rel="stylesheet" href="/central/tema/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="/central/tema/css/bootstrap-theme.min.css">-->
-        <link rel="stylesheet" href="/central/tema/css/main.css">		
-        <link rel="stylesheet" href="/central/tema/css/font-awesome.css">		
+	<!-- <link rel="stylesheet" href="/central/tema/css/bootstrap-theme.min.css">-->
+	<link rel="stylesheet" href="/central/tema/css/main.css">		
+	<link rel="stylesheet" href="/central/tema/css/font-awesome.css">		
         
 
 	  <!--	<link rel="stylesheet" rev="stylesheet" href="../css/template.css" type="text/css" media="screen"/>-->
